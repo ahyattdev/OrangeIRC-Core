756746af35064d467fb13cea8bfa3b6d3420178d
Andrew Hyatt <ahyattdev@icloud.com>
2016-06-28 17:04:17 -0700

    Initial commit

---
+++ OrangeIRC Core/OrangeIRC Core.h
@@ -0,0 +1,19 @@
+//
+//  OrangeIRC Core.h
+//  OrangeIRC Core
+//
+//  Created by Andrew Hyatt on 6/28/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OrangeIRC Core.
+FOUNDATION_EXPORT double OrangeIRC_CoreVersionNumber;
+
+//! Project version string for OrangeIRC Core.
+FOUNDATION_EXPORT const unsigned char OrangeIRC_CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OrangeIRC_Core/PublicHeader.h>
+
+
