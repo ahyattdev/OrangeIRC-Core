bdeb9c1c42c97759976bb5edd5c5d14068e9e5c0
Andrew Hyatt <ahyattdev@icloud.com>
2016-07-03 21:25:38 -0700

    User interface improvements

---
+++ OrangeIRC iOS/RoomViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RoomViewController.swift
+//  OrangeIRC
+//
+//  Created by Andrew Hyatt on 7/3/16.
+//
+//
+
+import UIKit
+import OrangeIRCCore
+
+public class RoomViewController : UITableViewController, UISplitViewControllerDelegate {
+    
+    
+}
