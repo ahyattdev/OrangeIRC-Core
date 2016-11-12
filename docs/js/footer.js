af182e0d421332ada612dacff910a3c394076cc6
Andrew Hyatt <ahyattdev@icloud.com>
2016-11-12 00:29:41 -0800

    Add GitHub pages site

---
+++ docs/js/footer.js
@@ -0,0 +1,6 @@
+document.getElementById("footer").innerHTML =
+`
+<div class="container">
+<p>Created by <a href="https://github.com/ahyattdev">Andrew Hyatt</a> using <a href="https://getbootstrap.com">Bootstrap</a>.</p>
+</div>
+`;
---
+++ docs/js/header.js
@@ -0,0 +1,22 @@
+document.getElementById("header").innerHTML =
+`
+<nav class="navbar navbar-default">
+<div class="container-fluid">
+
+<div class="navbar-header">
+    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="/">Andrew Hyatt</a>
+</div>
+<div class="collapse navbar-collapse" id="navbar-collapse">
+<ul class="nav navbar-nav">
+
+</ul>
+</div>
+</div>
+</nav>
+`;
