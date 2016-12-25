e5f6183a6842388cedfd8fa94a13825aee903e44
Andrew Hyatt <ahyattdev@icloud.com>
2016-12-25 11:55:52 -0800

    Add unit tests

---
+++ OrangeIRC Core Tests/MessageTests.swift
@@ -0,0 +1,28 @@
+//
+//  CommandTests.swift
+//  OrangeIRC
+//
+//  Created by Andrew Hyatt on 12/25/16.
+//
+//
+
+import XCTest
+import OrangeIRCCore
+
+class MessageTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testIRCv3Tags() {
+        //let msg = Message("@badges=staff/1,bits/1000;bits=100;color=;display-name=TWITCH_UserNaME;emotes=;id=b34ccfc7-4977-403a-8a94-33c6bac34fb8;mod=0;room-id=1337;subscriber=0;turbo=1;user-id=1337;user-type=staff :twitch_username!twitch_username@twitch_username.tmi.twitch.tv PRIVMSG #channel :cheer100")
+    }
+    
+}
