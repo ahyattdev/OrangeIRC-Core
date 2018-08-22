af3bf1070158e151bcc184ca15e4a6b202cf9478
Andrew Hyatt <ahyattdev@icloud.com>
2018-08-22 11:21:09 -0400

    Generate documentation
    Doesn't help since nothing is documented yet 🙃

---
+++ docs.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#  docs.sh
+#  OrangeIRC
+#
+#  Created by Andrew Hyatt on 8/22/18.
+#  
+
+jazzy \
+-m "OrangeIRCCore" \
+-a "Andrew Hyatt" \
+-u https://ahyattdev.github.io \
+-g https://github.com/ahyattdev/OrangeIRC \
+-r https://ahyattdev.github.io/OrangeIRC \
