07464b01ec771905fc8810b833c49344917075f0
Andrew Hyatt <ahyattdev@icloud.com>
2018-09-07 11:13:28 -0400

    Adopt GPL

--- OrangeIRC Core/LogEvent.swift
+++ OrangeIRC Core/LogEvent.swift
@@ -1,10 +1,19 @@
+// This file of part of the Swift IRC client framework OrangeIRC Core.
 //
-//  Event.swift
-//  OrangeIRC
+// Copyright © 2016 Andrew Hyatt
 //
-//  Created by Andrew Hyatt on 6/28/16.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Necessary because of UIFont and NSFont
 #if os(iOS) || os(tvOS)
